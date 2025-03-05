d9db7fe74c2ed5d37af475301d12d3beacf91d19
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-03-05 19:58:49 +0530

    Merge interval Leetcode: 56

---
+++ Merge_Intervals.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        vector<vector<int>> merge(vector<vector<int>>& intervals) {
+            sort(intervals.begin(),intervals.end());
+            int start=intervals[0][0];
+            int end=intervals[0][1];
+            vector<vector<int>>ans;
+    
+            for(int i=1;i<intervals.size();i++){
+              
+                    if(intervals[i][0]<=end ){
+                        end=max(intervals[i][1],end);
+                        start=min(start,intervals[i][0]);
+                    }else{
+                        ans.push_back({start,end});
+                        start=intervals[i][0];
+                        end=intervals[i][1];
+                       
+                    }
+            }
+            ans.push_back({start,end});
+            return ans;
+        }
+    };
+
