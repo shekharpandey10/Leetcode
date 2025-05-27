53cc0bea225dda94cefdd25432529e4b6125f130
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-05-27 20:13:55 +0530

    add recursion topic and qs subset sum

---
+++ recursion/subSetSums.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+   void solve(int i,vector<int>& arr,int n,vector<int>& ans,int sum){
+        if(i>=n){
+            ans.push_back(sum);
+            return;
+        }
+        solve(i+1,arr,n,ans,sum+arr[i]);
+        solve(i+1,arr,n,ans,sum);
+    }
+    vector<int> subsets(vector<int>&arr){
+        
+        int i=0;
+        vector<int>ans;
+        int sum=0;
+        int n=arr.size();
+        
+        solve(i,arr,n,ans,sum);
+        return ans;
+        
+    }
+    vector<int> subsetSums(vector<int>& arr) {
+        // code here
+        
+       return subsets(arr);
+    }
+};  
