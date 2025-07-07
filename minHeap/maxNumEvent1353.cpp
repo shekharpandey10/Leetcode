711f5f94b8ad70d20df131e2acb55f2aa29a1682
Chandra shekhar pandey <158845488+shekharpandey10@users.noreply.github.com>
2025-07-08 01:45:51 +0530

    Leetcode: 1353. Maximum Number of Events That Can Be Attended

---
+++ minHeap/maxNumEvent1353.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+       int n=events.size();
+        sort(events.begin(),events.end());
+      int i=0;
+      int count=0;
+      int day=events[0][0];
+        priority_queue<int,vector<int>,greater<int>>pq;
+
+        while( !pq.empty() ||i<n){
+                if(pq.empty()){
+                    day=events[i][0];
+                }
+            while(i<n && events[i][0]==day){
+                    pq.push(events[i][1]);
+                    i++;
+            }
+            if(!pq.empty()){
+                pq.pop();
+                count++;
+            }
+            day++;
+
+            while(!pq.empty() &&pq.top()<day){
+                pq.pop();
+
+            }
+        }
+        return count;
+
+    }
+};
